c16e77af47c601277ec0a5ef41edbd7abaf62142
DMNhut <ct7sxhctqk@privaterelay.appleid.com>
2026-06-29 20:23:51 +0700

    Cap nhat va xoa san pham tren PocketBase

--- lib/ui/products/products_manager.dart
+++ lib/ui/products/products_manager.dart
@@ -37,14 +37,18 @@ class ProductsManager with ChangeNotifier {
   Future<void> updateProduct(Product product) async {
     final index = _items.indexWhere((item) => item.id == product.id);
     if (index >= 0) {
-      _items[index] = product;
+      final updatedProduct = await _productsService.updateProduct(product);
+      if (updatedProduct != null) {
+        _items[index] = updatedProduct;
+        notifyListeners();
+      }
       notifyListeners();
     }
   }
 
-  void deleteProduct(String id) {
+  Future<void> deleteProduct(String id) async {
     final index = _items.indexWhere((item) => item.id == id);
-    if (index >= 0) {
+    if (index >= 0 && await _productsService.deleteProduct(id)) {
       _items.removeAt(index);
       notifyListeners();
     }
